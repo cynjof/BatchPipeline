5ddfc4dc035f99ea0629ae110f36a3d58b0ebac7
cynjof <79277663+cynjof@users.noreply.github.com>
2024-09-04 12:53:20 +0000

    test dagbag moved

--- test/dags/test_dag_validity.py
+++ test/dags/test_dag_validity.py
@@ -1,4 +1,10 @@
+import pytest
 from airflow.models import DagBag
+
+
+@pytest.fixture()
+def dagbag():
+    return DagBag()
 
 
 def test_no_import_errors(monkeypatch):
@@ -7,6 +13,6 @@ def test_no_import_errors(monkeypatch):
     monkeypatch.setenv("AIRFLOW_VAR_BUCKET", "test-bucket")
     monkeypatch.setenv("AIRFLOW_VAR_EMR_ID", "test-emr-id")
 
-    dag_bag = DagBag()
+    dag_bag = dagbag()
     assert len(dag_bag.import_errors) == 0, "No Import Failures"
     assert dag_bag.size() == 1
